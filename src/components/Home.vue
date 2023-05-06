570584e40e5edc154af956ea91858760a7875c24
Liliya Semenova <lili-g-semenova@yandex.ru>
2023-05-06 21:38:21 +0300

    Вёрстка главной страницы

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,137 +1,144 @@
 <template> 
   <div >
     <div>
-    <ul class="nav" >
-      <li class="nav-item h4 ml-4" >
-        <i class="bi bi-facebook"></i>
+    <!-- ШАПКА -->
+      <div class="d-flex justify-content-around">
+
+        <div class="nav py-4">          
+          <li class="nav-item h4 ml-4" >
+            <i class="bi bi-facebook"></i>
+          </li>
+          <li class="nav-item h4 ml-4">
+            <i class="bi bi-instagram"></i>
+          </li>
+          <li class="nav-item h4 ml-4">
+            <i class="bi bi-facebook"></i>
+          </li>   
+        </div>
+
+        <div class="nav py-4"> 
+        <img alt="Никольский молл" src="../assets/logo.png"  class="img-responsive" width="320"/>  
+        </div>
+
+        <div class="nav  py-4">
+            <li class="nav-item h4 ml-4" >
+              <i class="bi bi-search"></i>
+            </li>
+            <li class="nav-item h4 ml-4">
+              <i class="bi bi-geo-alt-fill"></i>
+            </li>
+            <li class="nav-item h4 ml-4">
+              <i class="bi bi-telephone-fill"></i>
+            </li>     
+        </div>
+      </div>
+ 
+    <!-- НАВИГАЦИЯ -->
+   <ul class="nav justify-content-center">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">Магазины</a>
       </li>
-      <li class="nav-item h4 ml-4">
-        <i class="bi bi-instagram"></i>
+      <li class="nav-item">
+        <a class="nav-link" href="#">О нас</a>
       </li>
-      <li class="nav-item h4 ml-4">
-        <i class="bi bi-facebook"></i>
-      </li>     
+      <li class="nav-item">
+        <a class="nav-link" href="#">Новости и Акции</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Как добраться</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Партнерам</a>
+      </li>
     </ul>
-  </div>
 
-  <div>
-     <img alt="Никольский молл" src="../assets/logo.png"  class="img-responsive" width="320"/>  
-    </div>
-
-
-      <div class="ml-4">
-      <ul class="nav" >
-        <li class="nav-item h4 ml-4" >
-         <i class="bi bi-search"></i>
-        </li>
-        <li class="nav-item h4 ml-4">
-          <i class="bi bi-geo-alt"></i>
-        </li>
-        <li class="nav-item h4 ml-4">
-          <i class="bi bi-telephone"></i>
-        </li>     
-      </ul>
-    </div>
-  
-    
-    <b-icon icon="search"></b-icon>
-    <b-icon icon="geo-alt-fill"></b-icon>
-    <b-icon icon="telephone-fill"></b-icon>    
-    
-    <b-nav tabs align="center">
-      <b-nav-item active>Магазины</b-nav-item>
-      <b-nav-item>О нас</b-nav-item>
-      <b-nav-item>Новости и Акции </b-nav-item>
-      <b-nav-item>Как добраться </b-nav-item>
-      <b-nav-item disabled>Партнерам</b-nav-item>
-    </b-nav>
-    <img alt="Никольский молл" fluid src="../assets/Frame.jpg"/>
-
-
-    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <!-- КАРУСЕЛЬ -->
+    <div id="carouselExampleIndicators" class="carousel slide container-fluid" data-ride="carousel" style="height: 673px; background-color: #EDFCFF">
       <ol class="carousel-indicators">
         <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
         <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
         <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
       </ol>
-      <div class="carousel-inner">
+      <div class="carousel-inner" style="height: 673px">
         <div class="carousel-item active">
-          <img src="../assets/Childs_World.png" class="d-block w-100" alt="...">
+          <img src="../assets/Frame.jpg" alt="Первый слайд">
         </div>
         <div class="carousel-item">
-          <img src="../assets/Childs_World.png" class="d-block w-100" alt="...">
+          <img class="d-block w-100" src="../assets/Kiehls.png" alt="Второй слайд">
         </div>
         <div class="carousel-item">
-          <img src="../assets/Childs_World.png" class="d-block w-100" alt="...">
+          <img class="d-block w-100" src="../assets/Frame.jpg" alt="Третий слайд">
         </div>
       </div>
-      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev" style="height: 673px">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Предыдущий</span>
+        <span class="sr-only">Previous</span>
       </a>
-      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next" style="height: 673px">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Следующий</span>
+        <span class="sr-only">Next</span>
       </a>
     </div>
-
-
-
-
-    <!-- <div class="album py-5 bg-light">
-      <div class="container">
-
-        <div>
-          <b-card-group deck class="mb-4">
-
-            <div class="card" >
-              <img src="../assets/Childs_World.png" class="card-img p-5" alt="Детский мир">
-              <p class="card-text">3 этаж </p>
-            </div>    
-
-            <div class="card" >
-              <img src="../assets/Kiehls.png" class="card-img p-5" alt="Детский мир">
-              <p class="card-text">3 этаж </p>
-            </div>    
-
-            <div class="card" >
-              <img src="../assets/Mixit.png" class="card-img p-5" alt="Детский мир">
-              <p class="card-text">3 этаж </p>
-            </div>    
-
-            <div class="card" >
-              <img src="../assets/Respect.png" class="card-img p-5" alt="Детский мир">
-              <p class="card-text">3 этаж </p>
-            </div>    
-          </b-card-group>
-
-          <b-card-group deck>
-
-            <div class="card" style="width: 18rem">
-              <img src="../assets/Childs_World.png" class="card-img p-5" alt="Детский мир">
-              <p class="card-text">3 этаж </p>
-            </div>    
-
-            <div class="card" >
-              <img src="../assets/Kiehls.png" class="card-img p-5" alt="Детский мир">
-              <p class="card-text">3 этаж </p>
-            </div>    
-
-            <div class="card" >
-              <img src="../assets/Mixit.png" class="card-img p-5" alt="Детский мир">
-              <p class="card-text">3 этаж </p>
-            </div>    
-
-            <div class="card" >
-              <img src="../assets/Respect.png" class="card-img p-5" alt="Детский мир">
-              <p class="card-text">3 этаж </p>
-            </div>    
-          </b-card-group>
-        </div>
+    <div>     
+    </div>
+    </div>
   
+    <div class="container">
+      <!-- ПЕРВЫЙ РЯД -->
+      <div class="row">
+        <div class="col-sm ">
+          <div class="card" >
+          <img src="../assets/Childs_World.png" class="card-img p-5" alt="Детский мир">
+              <p class="card-text">3 этаж </p>
+            </div>
+        </div>
+        <div class="col-sm">
+          <div class="card" >
+          <img src="../assets/Kiehls.png" class="card-img p-5" alt="Детский мир">
+          <p class="card-text">3 этаж </p>
+          </div>
+        </div>
+        <div class="col-sm">
+          <div class="card" >
+          <img src="../assets/Mixit.png" class="card-img p-5" alt="Детский мир">
+              <p class="card-text">3 этаж </p>
+            </div>
+        </div>
+        <div class="col-sm">
+          <div class="card" >
+          <img src="../assets/Respect.png" class="card-img p-5" alt="Детский мир">
+              <p class="card-text">3 этаж </p>
+            </div>
+        </div>
       </div>
-   
-  </div> -->
+<!-- ВТОРОЙ РЯД -->
+      <div class="row">
+        <div class="col-sm ">
+          <div class="card" >
+          <img src="../assets/Childs_World.png" class="card-img p-5" alt="Детский мир">
+              <p class="card-text">3 этаж </p>
+            </div>
+        </div>
+        <div class="col-sm">
+          <div class="card" >
+          <img src="../assets/Kiehls.png" class="card-img p-5" alt="Детский мир">
+          <p class="card-text">3 этаж </p>
+          </div>
+        </div>
+        <div class="col-sm">
+          <div class="card" >
+          <img src="../assets/Mixit.png" class="card-img p-5" alt="Детский мир">
+              <p class="card-text">3 этаж </p>
+            </div>
+        </div>
+        <div class="col-sm">
+          <div class="card" >
+          <img src="../assets/Respect.png" class="card-img p-5" alt="Детский мир">
+              <p class="card-text">3 этаж </p>
+            </div>
+        </div>
+      </div>
+    </div>
 
   <p class=" pt-3">Смотреть ещё 37 магазинов</p>
 
--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './bootstrap'
-import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+// import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 
 
 Vue.config.productionTip = false
 
-Vue.use(BootstrapVue)
-Vue.use(BootstrapVueIcons)
+// Vue.use(BootstrapVue)
+// Vue.use(BootstrapVueIcons)
 
 new Vue({
   render: h => h(App),
